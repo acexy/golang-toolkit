c72c750cb8be25f7e6474e1dc46ead47e1926c8e
Acexy <thankjava@gmail.com>
2023-08-03 16:06:59 +0800

    增加json功能

--- util/json.go
+++ util/json.go
@@ -2,15 +2,45 @@ package util
 
 import "encoding/json"
 
-func ToJsonString(any any) (str string, err error) {
+// ToJson 转json字符串 忽略任何错误
+func ToJson(any any) (json string) {
+	json, _ = ToJsonError(any)
+	return
+}
+
+// ParseJson 转对象 忽略任何错误
+func ParseJson(jsonString string, any any) {
+	_ = ParseJsonError(jsonString, any)
+}
+
+// ToJsonError 转json字符串 返回任何错误
+func ToJsonError(any any) (jsonString string, err error) {
 	bytes, err := json.Marshal(any)
 	if err != nil {
 		return
 	}
-	str = string(bytes)
+	jsonString = string(bytes)
 	return
 }
 
-func ParseJsonObject(jsonString string, any any) error {
+// ParseJsonError 转对象 返回任何错误
+func ParseJsonError(jsonString string, any any) error {
 	return json.Unmarshal([]byte(jsonString), any)
+}
+
+// ToJsonPanic 转json字符串 任何错误将触发panic
+func ToJsonPanic(any any) (jsonString string) {
+	jsonString, err := ToJsonError(any)
+	if err != nil {
+		panic(err)
+	}
+	return
+}
+
+// ParseJsonPanic 转对象 任何错误将触发panic
+func ParseJsonPanic(jsonString string, any any) {
+	err := ParseJsonError(jsonString, any)
+	if err != nil {
+		panic(err)
+	}
 }
