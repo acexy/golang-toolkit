3b25b9c14b023077e61f6e1a80230704c0c4267f
Acexy <thankjava@gmail.com>
2023-09-07 20:29:55 +0800

    增加缓存模块

--- cache/bigcache.go
+++ cache/bigcache.go
@@ -2,30 +2,43 @@ package cache
 
 import (
 	"context"
+	"github.com/acexy/golang-toolkit/util"
 	"github.com/allegro/bigcache/v3"
-	"time"
 )
 
-type BigCache struct {
-	CachingBucket
+type BigCacheBucket struct {
+	cache *bigcache.BigCache
 }
 
-func (b *BigCache) BucketName() string {
-	return ""
-}
-func (b *BigCache) Get(key string) (any, error) {
-	return nil, nil
-}
-func (b *BigCache) Put(key string, data any) error {
-	return nil
-}
-func (b *BigCache) Evict(key string) error {
-	return nil
-}
-
-func newBigCache() *CachingBucket {
-	cache, _ := bigcache.New(context.Background(), bigcache.DefaultConfig(10*time.Minute))
-	return &CachingBucket{
-		cache: cache,
+func (b *BigCacheBucket) Get(key string, result any) error {
+	v, err := b.cache.Get(key)
+	if err != nil {
+		return err
 	}
+	err = util.ParseJsonError(string(v), result)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (b *BigCacheBucket) Put(key string, data any) error {
+	bytes, err := util.ToJsonBytesError(data)
+	if err != nil {
+		return err
+	}
+	err = b.cache.Set(key, bytes)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (b *BigCacheBucket) Evict(key string) error {
+	return b.cache.Delete(key)
+}
+
+func NewBigCacheBucket(config bigcache.Config) *BigCacheBucket {
+	cache, _ := bigcache.New(context.Background(), config)
+	return &BigCacheBucket{cache: cache}
 }
--- cache/cache.go
+++ cache/cache.go
@@ -1,38 +1,53 @@
 package cache
 
 import (
+	"errors"
 	"github.com/acexy/golang-toolkit/log"
 	"sync"
 )
 
 var cacheOnce sync.Once
-var caches map[string]*CachingBucket
-var rw sync.RWMutex
 
-type CachingBucket struct {
-	cache any
+type CachingManager struct {
+	rw     sync.RWMutex
+	caches map[string]CachingBucket
 }
 
-type Cache interface {
-	BucketName() string
-	Get(key string) (any, error)
+type CachingBucket interface {
+
+	// Get 获取指定key对应的值
+	// result 值类型指针
+	Get(key string, result any) error
+
+	// Put 设置key对应值
 	Put(key string, data any) error
+
+	// Evict 清除缓存
 	Evict(key string) error
 }
 
-func NewBigCache(bucketName string) *CachingBucket {
-	var cache *CachingBucket
+func NewCacheBucketManager(bucketName string, bucket CachingBucket) *CachingManager {
+	var cachingManager *CachingManager
 	cacheOnce.Do(func() {
-		cache = &CachingBucket{}
-		caches = make(map[string]*CachingBucket, 1)
-		caches[bucketName] = newBigCache()
+		cachingManager = &CachingManager{
+			caches: make(map[string]CachingBucket),
+		}
+		cachingManager.caches[bucketName] = bucket
 	})
 	log.Logrus().Warnln("bigCache repeated initialization")
-	return cache
+	return cachingManager
 }
 
-func (c *CachingBucket) GetBucket(bucketName string) *CachingBucket {
-	rw.RLock()
-	defer rw.RUnlock()
-	return caches[bucketName]
+func (c *CachingManager) GetBucket(bucketName string) CachingBucket {
+	c.rw.RLock()
+	defer c.rw.RUnlock()
+	return c.caches[bucketName]
+}
+
+func (c *CachingManager) Get(bucketName, key string, result any) error {
+	bucket := c.GetBucket(bucketName)
+	if bucket == nil {
+		return errors.New("bad bucketName " + bucketName)
+	}
+	return bucket.Get(key, result)
 }
