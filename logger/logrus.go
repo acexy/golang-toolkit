2d2977659944741f573ab524dbda646d802d744e
Acexy <thankjava@gmail.com>
2024-03-20 15:18:24 +0800

    fix: log name

--- logger/logrus.go
+++ logger/logrus.go
@@ -51,7 +51,7 @@ func enableFile(level logrus.Level, formatter logrus.Formatter, fileConfig ...*l
 	fileLogger.SetFormatter(formatter)
 	if len(fileConfig) == 0 {
 		fileLogger.SetOutput(&lumberjack.Logger{
-			Filename:   "./logrus.logger",
+			Filename:   "./logrus.log",
 			MaxSize:    200,
 			MaxBackups: 100,
 			MaxAge:     365,
