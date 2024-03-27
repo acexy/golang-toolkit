4d7d83a16ab1cd523dc9ea496fff2a678c5d929b
Acexy <thankjava@gmail.com>
2024-03-27 15:00:18 +0800

    增加字符串工具集

---
+++ util/str/stringutil.go
@@ -0,0 +1,60 @@
+package str
+
+import "strings"
+
+// CharLength 返回字符串的字符长度
+func CharLength(str string) int {
+	return len([]rune(str))
+}
+
+// Substring 字符串截取指定长度 包含start下标，不含end下标
+func Substring(str string, start, end int) string {
+	raw := []rune(str)
+	return string(raw[start:end])
+}
+
+// HasText 检查是否具有有意义的内容
+func HasText(str string) bool {
+	return CharLength(strings.TrimSpace(str)) > 0
+}
+
+// HasLength 检查是否没有任何长度的值
+func HasLength(str string) bool {
+	return str != ""
+}
+
+type Builder struct {
+	b strings.Builder
+}
+
+// NewBuilder 创建一个StringBuilder
+func NewBuilder(str ...string) *Builder {
+	builder := &Builder{}
+	if len(str) > 0 {
+		builder.b.WriteString(str[0])
+	}
+	return builder
+}
+
+func (b *Builder) WriteString(str string) *Builder {
+	b.b.WriteString(str)
+	return b
+}
+
+func (b *Builder) WriteByte(byte byte) *Builder {
+	_ = b.b.WriteByte(byte)
+	return b
+}
+
+func (b *Builder) WriteBytes(bytes []byte) *Builder {
+	b.b.Write(bytes)
+	return b
+}
+func (b *Builder) WriteRune(r rune) *Builder {
+	b.b.WriteRune(r)
+	return b
+}
+
+func (b *Builder) ToString() string {
+	return b.b.String()
+}
