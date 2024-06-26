ea257a1e62ec9218a2c3baf2f26716142fee462e
Acexy <thankjava@gmail.com>
2024-06-26 14:29:59 +0800

    add netutil

--- util/net/netutil.go
+++ util/net/netutil.go
@@ -7,7 +7,7 @@ import (
 
 // Telnet 尝试在指定的时间内连接指定主机的指定端口 用于探活网络端口 address: ip:port
 func Telnet(address string, timeout time.Duration) bool {
-	conn, err := net.DialTimeout("tcp", address, timeout) // 2秒超时
+	conn, err := net.DialTimeout("tcp", address, timeout)
 	if err != nil {
 		return false
 	}
