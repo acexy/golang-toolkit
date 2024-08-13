d9052a67cb9d10d185894872cb4c888d1b260465
Acexy <thankjava@gmail.com>
2024-08-13 16:31:14 +0800

    新增随机同概率百分比返回结果

--- math/random/number.go
+++ math/random/number.go
@@ -18,14 +18,17 @@ type seedRandom struct {
 	rand *rand.Rand
 }
 
+// NewRandom 创建随机数生成器 指定随机算法的种子
 func NewRandom(seed int64) *seedRandom {
 	return &seedRandom{rand: rand.New(rand.NewSource(seed))}
 }
 
+// RandInt 生成指定范围内随机数 [0,max]
 func (s *seedRandom) RandInt(max int) int {
 	return s.rand.Intn(max)
 }
 
+// RandRangeInt 生成指定范围内随机数 [min,max]
 func (s *seedRandom) RandRangeInt(min, max int) int {
 	return s.rand.Intn(max-min+1) + min
 }
