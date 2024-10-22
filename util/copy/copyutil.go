f294aa3f29ed12c56bdac65bf45960ea7bcb1970
Acexy <thankjava@gmail.com>
2024-10-22 17:43:13 +0800

    json支持通过json.Timestamp自动序列化/反序列化time.Time <-> 时间戳

--- util/copy/copyutil.go
+++ util/copy/copyutil.go
@@ -19,43 +19,39 @@ func Copy(toValue interface{}, fromValue interface{}) error {
 func convertExtendsFields(dst interface{}, src interface{}) error {
 	dstValue := reflect.ValueOf(dst).Elem()
 	srcValue := reflect.ValueOf(src).Elem()
-
 	for i := 0; i < dstValue.NumField(); i++ {
 		dstField := dstValue.Field(i)
 		srcField := srcValue.Field(i)
-
 		if dstField.CanSet() && srcField.IsValid() && !srcField.IsZero() {
-			if !srcField.IsZero() {
-				if srcField.Type() == reflect.TypeOf(time.Time{}) && dstField.Type() == reflect.TypeOf(json.Timestamp{}) {
-					dstField.Set(reflect.ValueOf(json.Timestamp{Time: srcField.Interface().(time.Time)}))
-				} else if srcField.Type() == reflect.PtrTo(reflect.TypeOf(time.Time{})) && dstField.Type() == reflect.PtrTo(reflect.TypeOf(json.Timestamp{})) {
-					if !srcField.IsNil() {
-						timeValue := *srcField.Interface().(*time.Time)
-						dstField.Set(reflect.ValueOf(&json.Timestamp{Time: timeValue}))
-					}
-				} else if srcField.Type() == reflect.PtrTo(reflect.TypeOf(time.Time{})) && dstField.Type() == reflect.TypeOf(json.Timestamp{}) {
-					if !srcField.IsNil() {
-						timeValue := *srcField.Interface().(*time.Time)
-						dstField.Set(reflect.ValueOf(json.Timestamp{Time: timeValue}))
-					}
-				} else if srcField.Type() == reflect.TypeOf(time.Time{}) && dstField.Type() == reflect.PtrTo(reflect.TypeOf(json.Timestamp{})) {
-					timeValue := srcField.Interface().(time.Time)
+			if srcField.Type() == reflect.TypeOf(time.Time{}) && dstField.Type() == reflect.TypeOf(json.Timestamp{}) {
+				dstField.Set(reflect.ValueOf(json.Timestamp{Time: srcField.Interface().(time.Time)}))
+			} else if srcField.Type() == reflect.PtrTo(reflect.TypeOf(time.Time{})) && dstField.Type() == reflect.PtrTo(reflect.TypeOf(json.Timestamp{})) {
+				if !srcField.IsNil() {
+					timeValue := *srcField.Interface().(*time.Time)
 					dstField.Set(reflect.ValueOf(&json.Timestamp{Time: timeValue}))
-				} else if srcField.Type() == reflect.TypeOf(json.Timestamp{}) && dstField.Type() == reflect.TypeOf(time.Time{}) {
-					dstField.Set(reflect.ValueOf(srcField.Interface().(json.Timestamp).Time))
-				} else if srcField.Type() == reflect.PtrTo(reflect.TypeOf(json.Timestamp{})) && dstField.Type() == reflect.PtrTo(reflect.TypeOf(time.Time{})) {
-					if !srcField.IsNil() {
-						timestampValue := srcField.Interface().(*json.Timestamp)
-						dstField.Set(reflect.ValueOf(&timestampValue.Time))
-					}
-				} else if srcField.Type() == reflect.TypeOf(json.Timestamp{}) && dstField.Type() == reflect.PtrTo(reflect.TypeOf(time.Time{})) {
-					timeValue := srcField.Interface().(json.Timestamp).Time
-					dstField.Set(reflect.ValueOf(&timeValue))
-				} else if srcField.Type() == reflect.PtrTo(reflect.TypeOf(json.Timestamp{})) && dstField.Type() == reflect.TypeOf(time.Time{}) {
-					if !srcField.IsNil() {
-						timeValue := srcField.Interface().(*json.Timestamp).Time
-						dstField.Set(reflect.ValueOf(timeValue))
-					}
+				}
+			} else if srcField.Type() == reflect.PtrTo(reflect.TypeOf(time.Time{})) && dstField.Type() == reflect.TypeOf(json.Timestamp{}) {
+				if !srcField.IsNil() {
+					timeValue := *srcField.Interface().(*time.Time)
+					dstField.Set(reflect.ValueOf(json.Timestamp{Time: timeValue}))
+				}
+			} else if srcField.Type() == reflect.TypeOf(time.Time{}) && dstField.Type() == reflect.PtrTo(reflect.TypeOf(json.Timestamp{})) {
+				timeValue := srcField.Interface().(time.Time)
+				dstField.Set(reflect.ValueOf(&json.Timestamp{Time: timeValue}))
+			} else if srcField.Type() == reflect.TypeOf(json.Timestamp{}) && dstField.Type() == reflect.TypeOf(time.Time{}) {
+				dstField.Set(reflect.ValueOf(srcField.Interface().(json.Timestamp).Time))
+			} else if srcField.Type() == reflect.PtrTo(reflect.TypeOf(json.Timestamp{})) && dstField.Type() == reflect.PtrTo(reflect.TypeOf(time.Time{})) {
+				if !srcField.IsNil() {
+					timestampValue := srcField.Interface().(*json.Timestamp)
+					dstField.Set(reflect.ValueOf(&timestampValue.Time))
+				}
+			} else if srcField.Type() == reflect.TypeOf(json.Timestamp{}) && dstField.Type() == reflect.PtrTo(reflect.TypeOf(time.Time{})) {
+				timeValue := srcField.Interface().(json.Timestamp).Time
+				dstField.Set(reflect.ValueOf(&timeValue))
+			} else if srcField.Type() == reflect.PtrTo(reflect.TypeOf(json.Timestamp{})) && dstField.Type() == reflect.TypeOf(time.Time{}) {
+				if !srcField.IsNil() {
+					timeValue := srcField.Interface().(*json.Timestamp).Time
+					dstField.Set(reflect.ValueOf(timeValue))
 				}
 			}
 		}
