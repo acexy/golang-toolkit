961249715b515d1ddccaca78f0bc7bae85f5c746
Acexy <thankjava@gmail.com>
2025-01-10 19:46:21 +0800

    增加百分比随机结果算法

--- math/random/misc.go
+++ math/random/misc.go
@@ -2,6 +2,8 @@ package random
 
 import (
 	"fmt"
+	"github.com/acexy/golang-toolkit/util/coll"
+	"github.com/shopspring/decimal"
 	"math"
 	"math/rand"
 	"strings"
@@ -26,7 +28,7 @@ func decimalPlaces(num float64) int {
 	return len(strings.TrimRight(decimalPart, "0"))
 }
 
-// ProbabilityTrue 支持任意小数精度的概率判断
+// ProbabilityTrue 随机执行指定概率(0-100%)返回true的计算
 // 0 <= percentage <= 100 超出范围将永远返回 false。
 func ProbabilityTrue(percentage float64) bool {
 	if percentage < 0 || percentage > 100 {
@@ -39,4 +41,43 @@ func ProbabilityTrue(percentage float64) bool {
 	defer randProbabilityPool.Put(r)
 	randomNumber := r.Intn(maxValue)
 	return randomNumber < threshold
+}
+
+// ProbabilityResult 按照设置的各种key的概率返回对应的key
+// 所有key的概率之和必须为100%
+func ProbabilityResult(percentage map[string]float64) string {
+	if len(percentage) == 0 {
+		return ""
+	}
+	total := decimal.Zero
+	for _, v := range percentage {
+		total = total.Add(decimal.NewFromFloat(v))
+	}
+	if total.Compare(decimal.NewFromFloat(100)) != 0 {
+		return ""
+	}
+	var maxScale int
+	coll.MapForeachAll(percentage, func(k string, v float64) {
+		maxScale = int(math.Max(float64(maxScale), float64(decimalPlaces(v))))
+	})
+	calcPercentage := coll.MapCollect(percentage, func(k string, v float64) (string, decimal.Decimal) {
+		return k, decimal.NewFromFloat(v).Mul(decimal.NewFromFloat(math.Pow(10, float64(maxScale))))
+	})
+	sum := decimal.Zero
+	coll.MapForeachAll(calcPercentage, func(k string, v decimal.Decimal) {
+		sum = sum.Add(v)
+	})
+	if sum.Compare(decimal.NewFromFloat(math.Pow(10, float64(maxScale))*100)) != 0 {
+		return ""
+	}
+	randomValue := rand.Intn(int(sum.IntPart() - 1)) // 随机数范围为 [0, total)
+	decimalRandomValue := decimal.NewFromInt(int64(randomValue))
+	cumulative := decimal.Zero
+	for key, scaledProb := range calcPercentage {
+		cumulative = cumulative.Add(scaledProb)
+		if decimalRandomValue.Compare(cumulative) < 0 {
+			return key
+		}
+	}
+	return ""
 }
--- math/random/misc_test.go
+++ math/random/misc_test.go
@@ -1,15 +1,36 @@
 package random
 
 import (
+	"github.com/acexy/golang-toolkit/util/coll"
 	"testing"
 )
 
 func TestProbabilityTrue(t *testing.T) {
 	var count int
 	for i := 0; i < 1000; i++ {
-		if ProbabilityTrue(50) {
+		if ProbabilityTrue(10) {
 			count++
 		}
 	}
 	print(count)
+}
+
+func TestProbabilityResult(t *testing.T) {
+	result := map[string]int{
+		"A": 0,
+		"B": 0,
+		"C": 0,
+		"D": 0,
+	}
+	for i := 0; i < 10000; i++ {
+		result[ProbabilityResult(map[string]float64{
+			"A": 10.15,
+			"B": 20.85,
+			"C": 53.05,
+			"D": 15.95,
+		})]++
+	}
+	coll.MapForeachAll(result, func(k string, v int) {
+		println(k, v)
+	})
 }
