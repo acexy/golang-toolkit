d281663bb1277efa219b7d544d8448a3d466abd8
Acexy <thankjava@gmail.com>
2025-04-29 16:11:38 +0800

    新增基础方法

---
+++ util/coll/map.go
@@ -0,0 +1,100 @@
+package coll
+
+// MapFirst 从map中抽取第一个元素
+func MapFirst[K comparable, V any](m map[K]V) (K, V) {
+	var key K
+	var value V
+	if len(m) == 0 {
+
+		return key, value
+	}
+	for k, v := range m {
+		key = k
+		value = v
+		return key, value
+	}
+	return key, value
+}
+
+// MapKeyToSlice 将map的key转换为slice
+func MapKeyToSlice[K comparable, V any](m map[K]V) []K {
+	if len(m) == 0 {
+		return nil
+	}
+	result := make([]K, 0)
+	for k, _ := range m {
+		result = append(result, k)
+	}
+	return result
+}
+
+// MapValueToSlice 将map的value转换为slice
+func MapValueToSlice[K comparable, V any](m map[K]V) []V {
+	if len(m) == 0 {
+		return nil
+	}
+	result := make([]V, 0)
+	for i := range m {
+		result = append(result, m[i])
+	}
+	return result
+}
+
+// MapCollect 将map转换成新的map
+func MapCollect[K, RK comparable, V, RV any](m map[K]V, mapFn func(K, V) (RK, RV)) map[RK]RV {
+	if len(m) == 0 {
+		return nil
+	}
+	result := make(map[RK]RV)
+	for k, v := range m {
+		key, value := mapFn(k, v)
+		result[key] = value
+	}
+	return result
+}
+
+// MapFilterCollect 将map过滤转换成新的map
+func MapFilterCollect[K, RK comparable, V, RV any](m map[K]V, mapFn func(K, V) (RK, RV, bool)) map[RK]RV {
+	if len(m) == 0 {
+		return nil
+	}
+	result := make(map[RK]RV)
+	for k, v := range m {
+		key, value, ok := mapFn(k, v)
+		if ok {
+			result[key] = value
+		}
+	}
+	return result
+}
+
+// MapFilterToSlice 将map过滤转换成切片
+func MapFilterToSlice[K comparable, V, R any](m map[K]V, mapFn func(K, V) (R, bool)) []R {
+	if len(m) == 0 {
+		return nil
+	}
+	result := make([]R, 0)
+	for k, v := range m {
+		n, f := mapFn(k, v)
+		if f {
+			result = append(result, n)
+		}
+	}
+	return result
+}
+
+// MapForeach 遍历map return false时停止迭代
+func MapForeach[K comparable, V any](m map[K]V, fn func(k K, v V) bool) {
+	for k, v := range m {
+		if !fn(k, v) {
+			return
+		}
+	}
+}
+
+// MapForeachAll 遍历map
+func MapForeachAll[K comparable, V any](m map[K]V, fn func(k K, v V)) {
+	for k, v := range m {
+		fn(k, v)
+	}
+}
---
+++ util/coll/slice_test.go
@@ -0,0 +1,252 @@
+package coll
+
+import (
+	"fmt"
+	"github.com/acexy/golang-toolkit/math/conversion"
+	"github.com/acexy/golang-toolkit/util/str"
+	"testing"
+)
+
+type people struct {
+	name string
+	age  int
+}
+
+func TestSliceContains(t *testing.T) {
+	intSlice := []int{1, 2, 3, 4, 5}
+	stringSlice := []string{"apple", "banana", "cherry"}
+
+	fmt.Println(SliceContains(intSlice, 3)) // 输出: true
+	fmt.Println(SliceIndexOf(intSlice, 3))
+	fmt.Println(SliceContains(intSlice, 6)) // 输出: false
+	fmt.Println(SliceIndexOf(intSlice, 6))
+	fmt.Println(SliceContains(stringSlice, "banana")) // 输出: true
+	fmt.Println(SliceContains(stringSlice, "grape"))  // 输出: false
+
+	peoples := []*people{
+		{name: "张三", age: 28},
+		{name: "李四", age: 20},
+		{name: "王五", age: 22},
+		{name: "赵六", age: 20},
+	}
+	fmt.Println(SliceContains(peoples, &people{name: "张三", age: 28}, func(a, b *people) bool {
+		return a.name == b.name && a.age == b.age
+	}))
+	list := []string{"US", ""}
+	fmt.Println(SliceContains(list, ""))
+}
+
+func TestSliceAnyContains(t *testing.T) {
+	peoples := []people{
+		{name: "张三", age: 28},
+		{name: "李四", age: 20},
+		{name: "王五", age: 22},
+		{name: "赵六", age: 20},
+	}
+	fmt.Println(SliceAnyContains(peoples, func(a people) bool {
+		return a.name == "张三"
+	}))
+	fmt.Println(SliceAnyIndexOf(peoples, func(a people) bool {
+		return a.name == "张三"
+	}))
+}
+
+func TestSliceFilter(t *testing.T) {
+	peoples := []people{
+		{name: "张三", age: 28},
+		{name: "李四", age: 20},
+		{name: "王五", age: 22},
+		{name: "赵六", age: 20},
+	}
+
+	peoplesPt := []*people{
+		{name: "张三", age: 28},
+		{name: "李四", age: 20},
+		{name: "王五", age: 22},
+		{name: "赵六", age: 20},
+	}
+	fmt.Println(SliceFilter(peoples, func(item people) bool {
+		return item.age == 20
+	}))
+	fmt.Println(SliceFilterFirstOne(peoplesPt, func(item *people) bool {
+		return item.age == 20
+	}))
+}
+func TestSliceIntersection(t *testing.T) {
+	intSlice1 := []int{3, 4, 4, 5, 6, 7}
+	intSlice2 := []int{1, 2, 3, 4, 3, 5}
+	fmt.Println(SliceIntersection(intSlice1, intSlice2))
+
+	intSlice3 := []int{1, 2, 3, 4, 5}
+	intSlice4 := []int{6, 6, 7, 7, 8, 9, 0}
+	fmt.Println(SliceIntersection(intSlice3, intSlice4))
+	peoples1 := []people{
+		{name: "张三", age: 28},
+		{name: "李四", age: 20},
+		{name: "王五", age: 22},
+		{name: "赵六", age: 20},
+	}
+	peoples2 := []people{
+		{name: "李四", age: 20},
+		{name: "赵六", age: 20},
+	}
+	fmt.Println(SliceIntersection(peoples1, peoples2, func(a, b people) bool {
+		return a.name == b.name && a.age == b.age
+	}))
+}
+
+func TestSliceUnion(t *testing.T) {
+	intSlice1 := []int{1, 2, 3, 4, 3, 5}
+	intSlice2 := []int{3, 4, 4, 5, 6, 7}
+	fmt.Println(SliceUnion(intSlice1, intSlice2))
+
+	peoples1 := []people{
+		{name: "张三", age: 28},
+		{name: "李四", age: 20},
+		{name: "王五", age: 22},
+		{name: "赵六", age: 20},
+		{name: "赵六", age: 21},
+	}
+	peoples2 := []people{
+		{name: "张三", age: 28},
+		{name: "李四", age: 20},
+		{name: "赵六", age: 20},
+		{name: "赵六", age: 21},
+		{name: "赵六", age: 21},
+	}
+	fmt.Println(SliceUnion(peoples1, peoples2, func(a, b people) bool {
+		return a.name == b.name && a.age == b.age
+	}))
+}
+
+func TestSliceSort(t *testing.T) {
+	peoples1 := []people{
+		{name: "张三", age: 28},
+		{name: "李四", age: 20},
+		{name: "王五", age: 22},
+		{name: "赵六", age: 20},
+		{name: "赵六", age: 21},
+	}
+	fmt.Println(peoples1)
+	SliceSort(peoples1, func(e people) int {
+		return e.age
+	}, true)
+	fmt.Println(peoples1)
+
+}
+
+func TestSliceComplement(t *testing.T) {
+	intSlice1 := []int{1, 2, 3, 4, 3, 5}
+	intSlice2 := []int{3, 4, 4, 5}
+	fmt.Println(SliceComplement(intSlice1, intSlice2))
+	peoples1 := []people{
+		{name: "张三", age: 28},
+		{name: "李四", age: 20},
+		{name: "王五", age: 22},
+		{name: "赵六", age: 20},
+		{name: "赵六", age: 21},
+	}
+	peoples2 := []people{
+		{name: "张三", age: 28},
+		{name: "赵六", age: 20},
+		{name: "赵六", age: 21},
+		{name: "赵六", age: 21},
+	}
+	fmt.Println(SliceComplement(peoples1, peoples2, func(a, b people) bool {
+		return a.name == b.name && a.age == b.age
+	}))
+}
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func TestSliceFilterToMap(t *testing.T) {
+	// 示例1: 整数切片
+	ints := []int{1, 2, 3, 4, 5, 6}
+	intMap := SliceFilterToMap(ints, func(t int) (string, int, bool) {
+		if t > 3 {
+			fromInt := conversion.FromInt(t)
+			return fromInt, t, true
+		}
+		return "", 0, false
+	})
+	fmt.Println(intMap)
+
+	// 示例2: 字符串切片
+	strings := []string{"apple", "banana", "cherry", "date"}
+	stringMap := SliceFilterToMap(strings, func(t string) (string, string, bool) {
+		if str.CharLength(t) > 4 {
+			return t, t, true
+		}
+		return "", "", false
+	})
+	fmt.Println(stringMap)
+
+	// 定义一个结构体切片
+	people := []*Person{
+		{Name: "Alice", Age: 25},
+		{Name: "Bob", Age: 30},
+		{Name: "Charlie", Age: 35},
+		{Name: "Dave", Age: 40},
+	}
+
+	// 调用通用方法
+	personMap := SliceFilterToMap(people, func(t *Person) (string, int, bool) {
+		if t.Age > 30 {
+			return t.Name, t.Age, true
+		}
+		return "", 0, false
+	})
+
+	// 打印结果
+	fmt.Println(personMap)
+}
+
+func TestSliceCollect(t *testing.T) {
+	// 输入切片
+	input := []Person{
+		{Name: "Alice", Age: 25},
+		{Name: "Bob", Age: 30},
+		{Name: "Charlie", Age: 35},
+	}
+
+	// 定义映射函数，用于从 Person 提取 Age
+	collect := func(p Person) int {
+		return p.Age
+	}
+
+	// 调用 SliceCollect
+	output := SliceCollect(input, collect)
+	fmt.Println(output)
+
+}
+
+func TestSliceDistinct(t *testing.T) {
+	input := []Person{
+		{Name: "Alice", Age: 35},
+		{Name: "Bob", Age: 30},
+		{Name: "Charlie", Age: 35},
+		{Name: "Charlie", Age: 35},
+	}
+	fmt.Println(SliceDistinct(input))
+
+	fmt.Println(SliceDistinctAny(input, func(t Person) int {
+		return t.Age
+	}))
+}
+func TestSliceGroupBy(t *testing.T) {
+	input := []Person{
+		{Name: "Alice", Age: 25},
+		{Name: "Bob", Age: 30},
+		{Name: "Charlie", Age: 35},
+		{Name: "Charlie", Age: 36},
+	}
+	fmt.Println(SliceGroupBy(input, func(t Person) string {
+		return t.Name
+	}))
+	fmt.Println(SliceAnyGroupBy(input, func(t Person) (string, int) {
+		return t.Name, t.Age
+	}))
+}
