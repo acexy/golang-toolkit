6bb9eaaee836d481054e8572794d547da851a8e4
Acexy <thankjava@gmail.com>
2025-09-28 15:20:08 +0800

    日志模块剥离sys组件防止固定引入第三方组件

--- sys/goroutine_test.go
+++ sys/goroutine_test.go
@@ -7,20 +7,22 @@ import (
 	"github.com/acexy/golang-toolkit/math/random"
 )
 
+var l = NewThreadLocal[string](func() string {
+	return random.UUID()
+})
+
 func TestLocal(t *testing.T) {
-	l := NewThreadLocal[string](func() string {
-		return random.UUID()
-	})
 	// 如果在获取线程本地变量之前，没有设置过，则返回默认值
 	fmt.Println(l.Get())
 	l.Set("123")
 	fmt.Println(l.Get())
-	f1(l)
+	f1()
+	f2()
 }
-func f1(local *ThreadLocal[string]) {
-	fmt.Println(local.Get())
+func f1() {
+	fmt.Println(l.Get())
 }
 
-func f2(local *ThreadLocal[string]) {
-	fmt.Println(local.Get())
+func f2() {
+	fmt.Println(l.Get())
 }
