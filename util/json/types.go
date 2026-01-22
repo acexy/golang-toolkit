ff29fb524c361de95254ea68f454395fd43a98d8
Acexy <thankjava@gmail.com>
2026-01-22 18:26:10 +0800

    调整目录结构

---
+++ util/json/types.go
@@ -0,0 +1,79 @@
+package json
+
+import (
+	"sync"
+	"time"
+)
+
+const (
+	// TimestampTypeMilli 时间戳类型 毫秒级别
+	TimestampTypeMilli TimeStampType = 0
+	// TimestampTypeSecond 时间戳类型 秒级别
+	TimestampTypeSecond TimeStampType = 1
+)
+
+var wrapperOnce sync.Once
+var defaultOptions = &TypeWrapperOptions{
+	// 默认毫秒
+	TimestampType: TimestampTypeMilli,
+}
+
+type TimeStampType int
+
+type TypeWrapperOptions struct {
+	// 序列化/反序列化 时间戳类型
+	TimestampType TimeStampType
+}
+
+type SetWrapperOption func(*TypeWrapperOptions)
+
+type Timestamp struct {
+	time.Time
+}
+
+func GlobalWrapperSetting(opt ...SetWrapperOption) {
+	wrapperOnce.Do(func() {
+		for _, o := range opt {
+			o(defaultOptions)
+		}
+	})
+}
+
+func Time2Timestamp(time time.Time) ([]byte, error) {
+	if time.IsZero() {
+		return ToBytesError(0)
+	}
+	var timestamp int64
+	if defaultOptions.TimestampType == TimestampTypeSecond {
+		timestamp = time.Unix()
+	} else if defaultOptions.TimestampType == TimestampTypeMilli {
+		timestamp = time.UnixMilli()
+	}
+	return ToBytesError(timestamp)
+}
+
+func Timestamp2Time(timestampBytes []byte) (time.Time, error) {
+	var timestamp int64
+	if err := ParseBytesError(timestampBytes, &timestamp); err != nil {
+		return time.Time{}, err
+	}
+	if defaultOptions.TimestampType == TimestampTypeSecond {
+		return time.Unix(timestamp, 0), nil
+	} else if defaultOptions.TimestampType == TimestampTypeMilli {
+		return time.UnixMilli(timestamp), nil
+	}
+	return time.Time{}, nil
+}
+
+func (t Timestamp) MarshalJSON() ([]byte, error) {
+	return Time2Timestamp(t.Time)
+}
+
+func (t *Timestamp) UnmarshalJSON(data []byte) error {
+	formatTime, err := Timestamp2Time(data)
+	if err != nil {
+		return err
+	}
+	t.Time = formatTime
+	return nil
+}
---
+++ util/net/ip/checker.go
@@ -0,0 +1,62 @@
+package ip
+
+import (
+	"net"
+
+	"github.com/acexy/golang-toolkit/logger"
+	"github.com/yl2chen/cidranger"
+)
+
+type IpChecker struct {
+	ranger cidranger.Ranger
+}
+
+func NewIpChecker(ruleCIDRIps ...string) *IpChecker {
+	ranger := cidranger.NewPCTrieRanger()
+	for _, ip := range ruleCIDRIps {
+		_, ipNet, err := net.ParseCIDR(ip)
+		if err != nil {
+			logger.Logrus().Warningln("parse ip error. ip:", ip)
+			continue
+		}
+		_ = ranger.Insert(cidranger.NewBasicRangerEntry(*ipNet))
+	}
+	return &IpChecker{
+		ranger: ranger,
+	}
+}
+
+// AddRuleIp 添加新的ip规则
+func (i *IpChecker) AddRuleIp(ruleCIDRIps ...string) {
+	if len(ruleCIDRIps) == 0 {
+		return
+	}
+	for _, ip := range ruleCIDRIps {
+		_, ipNet, err := net.ParseCIDR(ip)
+		if err != nil {
+			logger.Logrus().Warningln("parse ip error. ip:", ip)
+			continue
+		}
+		_ = i.ranger.Insert(cidranger.NewBasicRangerEntry(*ipNet))
+	}
+}
+
+// RemoveRuleIp 删除ip规则
+func (i *IpChecker) RemoveRuleIp(ruleCIDRIps ...string) {
+	for _, ip := range ruleCIDRIps {
+		_, ipNet, err := net.ParseCIDR(ip)
+		if err != nil {
+			logger.Logrus().Warningln("parse ip error. ip:", ip)
+			continue
+		}
+		_, err = i.ranger.Remove(*ipNet)
+		if err != nil {
+			logger.Logrus().Warningln("remove ip error. ip:", ip)
+		}
+	}
+}
+
+// Match 检测ip是否匹配
+func (i *IpChecker) Match(ip string) (bool, error) {
+	return i.ranger.Contains(net.ParseIP(ip))
+}
