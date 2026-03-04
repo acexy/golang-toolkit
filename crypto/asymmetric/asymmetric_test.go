95cbf303bccaca14ebdbbe4622e208768f284545
Acexy <thankjava@gmail.com>
2026-03-04 11:43:41 +0800

    调整ras算法保证线程安全的锁粒度

--- crypto/asymmetric/asymmetric_test.go
+++ crypto/asymmetric/asymmetric_test.go
@@ -14,9 +14,7 @@ import (
 )
 
 func TestRsaKey(t *testing.T) {
-	var manager = RsaKeyManager{
-		CreateSetting: CreateRsaSetting{Length: 2048},
-	}
+	var manager = NewRsaKeyManager(1024)
 	key, err := manager.Create()
 	if err != nil {
 		fmt.Println(err.Error())
